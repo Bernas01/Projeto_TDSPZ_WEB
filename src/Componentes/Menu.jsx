65e7da3e8ddc190425d8fac097c5d64ae28cce5d
Bernas01 <felipebernardes0108@gmail.com>
2023-10-30 22:42:16 -0300

    cp5

--- src/Componentes/Menu.jsx
+++ src/Componentes/Menu.jsx
@@ -18,7 +18,7 @@ function Menu() {
             <nav style={menu}>
                 <ul style={link}>
                     <li><a href="/" style={link}>HOME</a></li>
-                    <li><a href="/ListaCarros" style={link}>LISTA CARROS</a></li>
+                    <li><a href="/ListaCarros" style={link}>TECNOLOGIA</a></li>
                     <li><a href="/sobre" style={link}>SOBRE</a></li>
                 </ul>
             </nav>
